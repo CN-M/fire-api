6db7194b10377b24db0532ee4c1d9f26e0efc359
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-02-08 12:54:00 +0200

    Finished tutorial and mock api

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const app = require('express')();
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
+const express = require('express');
+const app = express();
 const PORT = 8080;
 
 app.listen(
     PORT,
     () => console.log(`It's alive on http://localhost:${PORT}`)
 )
+
+app.use( express.json() );
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'blue',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!'})
+    }
+
+    res.send({
+        tshirt: `shirt with your ${logo} and ID of ${id}`
+    })
+});
